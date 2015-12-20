c3ec9be8f0b21e09fa1cc716fcd4f72a2e320386
angeladai <angelaqdai@gmail.com>
2015-12-20 06:17:08 +0000

    DEBUG STATE (gpu version of lie convert)

--- Source/Solver/LieDerivUtil.h
+++ Source/Solver/LieDerivUtil.h
@@ -7,50 +7,129 @@
 #include <cutil_inline.h>
 #include <cutil_math.h>
 #include <device_functions.h>
+#include <math_constants.h>
 
 #include "../../SiftGPU/cuda_SimpleMatrixUtil.h"
 
-__inline__ __device__ float3x3 VectorToSkewSymmetricMatrix(const float3& v) {
-	float3x3 res; res.setZero();
-	res(1, 0) = v.z;
-	res(2, 0) = -v.y;
-	res(2, 1) = v.x;
-	res(0, 1) = -v.z;
-	res(0, 2) = v.y;
-	res(1, 2) = -v.x;
-	return res;
+#define ONE_TWENTIETH 0.05f
+#define ONE_SIXTH 0.16666667f
+
+//! compute a rotation exponential using the Rodrigues Formula.
+//		rotation axis w (theta = |w|); A = sin(theta) / theta; B = (1 - cos(theta)) / theta^2
+__inline__ __device__ void rodrigues_so3_exp(const float3& w, float A, float B, float3x3& R)
+{
+	{
+		const float wx2 = w.x * w.x;
+		const float wy2 = w.y * w.y;
+		const float wz2 = w.z * w.z;
+		R(0, 0) = 1.0f - B*(wy2 + wz2);
+		R(1, 1) = 1.0f - B*(wx2 + wz2);
+		R(2, 2) = 1.0f - B*(wx2 + wy2);
+	}
+	{
+		const float a = A*w.z;
+		const float b = B*(w.x * w.y);
+		R(0, 1) = b - a;
+		R(1, 0) = b + a;
+	}
+	{
+		const float a = A*w.y;
+		const float b = B*(w.x * w.z);
+		R(0, 2) = b + a;
+		R(2, 0) = b - a;
+	}
+	{
+		const float a = A*w.x;
+		const float b = B*(w.y * w.z);
+		R(1, 2) = b - a;
+		R(2, 1) = b + a;
+	}
 }
 
-__inline__ __device__ float3 SkewSymmetricMatrixToVector(const float3x3& m) {
-	return make_float3(m(2, 1), m(0, 2), m(1, 0));
+//! exponentiate a vector in the Lie algebra to generate a new SO3(a 3x3 rotation matrix).
+__inline__ __device__ float3x3 exp_rotation(const float3& w)
+{
+	const float theta_sq = dot(w, w);
+	const float theta = std::sqrt(theta_sq);
+	float A, B;
+	//Use a Taylor series expansion near zero. This is required for
+	//accuracy, since sin t / t and (1-cos t)/t^2 are both 0/0.
+	if (theta_sq < 1e-8) {
+		A = 1.0f - ONE_SIXTH * theta_sq;
+		B = 0.5f;
+	}
+	else {
+		if (theta_sq < 1e-6) {
+			B = 0.5f - 0.25f * ONE_SIXTH * theta_sq;
+			A = 1.0f - theta_sq * ONE_SIXTH*(1.0f - ONE_TWENTIETH * theta_sq);
+		}
+		else {
+			const float inv_theta = 1.0f / theta;
+			A = sin(theta) * inv_theta;
+			B = (1 - cos(theta)) * (inv_theta * inv_theta);
+		}
+	}
+
+	float3x3 result;
+	rodrigues_so3_exp(w, A, B, result);
+	return result;
 }
 
-//! exponential map: so(3) -> SO(3) / w -> R3x3
-__inline__ __device__ float3x3 LieAlgebraToLieGroupSO3(const float3& w) {
-	float3x3 res; res.setIdentity();
-	float norm = length(w);
-	if (norm < FLT_EPSILON)
-		return res;
-	float3x3 wHat = VectorToSkewSymmetricMatrix(w);
+//! logarithm of the 3x3 rotation matrix, generating the corresponding vector in the Lie Algebra
+__inline__ __device__ float3 ln_rotation(const float3x3& rotation)
+{
+	float3 result; // skew symm matrix = (R - R^T) * angle / (2 * sin(angle))
 
-	res += wHat * (sin(norm) / norm);
-	res += (wHat * wHat) * ((1.0f - cos(norm)) / (norm*norm));
-	return res;
-}
+	const float cos_angle = (rotation.trace() - 1.0f) * 0.5f;
+	//(R - R^T) / 2
+	result.x = (rotation(2, 1) - rotation(1, 2))*0.5f;
+	result.y = (rotation(0, 2) - rotation(2, 0))*0.5f;
+	result.z = (rotation(1, 0) - rotation(0, 1))*0.5f;
 
-//! logarithm map: SO(3) -> so(3) / R3x3 -> w
-__inline__ __device__ float3 LieGroupToLieAlgebraSO3(const float3x3& R) {
-	float tmp = (R.trace() - 1.0f) / 2.0f;
-
-	if (tmp < -1.0f)
-		tmp = -1.0f;
-	if (tmp > 1.0f)
-		tmp = 1.0f;
-	float angleOfRotation = acos(tmp);
-	if (angleOfRotation == 0.0f)
-		return make_float3(0.0f);
-	float3x3 lnR = (R - R.getTranspose()) * (angleOfRotation / (2.0f * sin(angleOfRotation)));
-	return SkewSymmetricMatrixToVector(lnR);
+	float sin_angle_abs = length(result); //sqrt(result*result);
+	if (cos_angle > (float)0.70710678118654752440) { // [0 - Pi/4[ use asin
+		if (sin_angle_abs > 0) {
+			result *= asin(sin_angle_abs) / sin_angle_abs;
+		}
+	}
+	else if (cos_angle > -(float)0.70710678118654752440) { // [Pi/4 - 3Pi/4[ use acos, but antisymmetric part
+		float angle = acos(cos_angle);
+		result *= angle / sin_angle_abs;
+	}
+	else
+	{  // rest use symmetric part
+		// antisymmetric part vanishes, but still large rotation, need information from symmetric part
+		const float angle = CUDART_PI_F - asin(sin_angle_abs);
+		const float
+			d0 = rotation(0, 0) - cos_angle,
+			d1 = rotation(1, 1) - cos_angle,
+			d2 = rotation(2, 2) - cos_angle;
+		float3 r2;
+		if (fabs(d0) > fabs(d1) && fabs(d0) > fabs(d2))
+		{ // first is largest, fill with first column
+			r2.x = d0;
+			r2.y = (rotation(1, 0) + rotation(0, 1))*0.5f;
+			r2.z = (rotation(0, 2) + rotation(2, 0))*0.5f;
+		}
+		else if (fabs(d1) > fabs(d2))
+		{ 			    // second is largest, fill with second column
+			r2.x = (rotation(1, 0) + rotation(0, 1))*0.5f;
+			r2.y = d1;
+			r2.z = (rotation(2, 1) + rotation(1, 2))*0.5f;
+		}
+		else
+		{							    // third is largest, fill with third column
+			r2.x = (rotation(0, 2) + rotation(2, 0))*0.5f;
+			r2.y = (rotation(2, 1) + rotation(1, 2))*0.5f;
+			r2.z = d2;
+		}
+		// flip, if we point in the wrong direction!
+		if (dot(r2, result) < 0)
+			r2 *= -1;
+		result = r2;
+		result *= (angle / length(r2));
+	}
+	return result;
 }
 
 __inline__ __device__ void matrixToPose(const float4x4& matrix, float3& rot, float3& trans, bool debug)
@@ -58,20 +137,28 @@ __inline__ __device__ void matrixToPose(const float4x4& matrix, float3& rot, flo
 	//!!!debugging
 	if (debug) printf("matrixToPose lie\n");
 	//!!!debugging
-	float3x3 R = matrix.getFloat3x3();
-	float3 tr = matrix.getTranslation();
+	
+	const float3x3 R = matrix.getFloat3x3();
+	const float3 t = matrix.getTranslation();
+	rot = ln_rotation(R);
+	const float theta = length(rot);
 
-	rot = LieGroupToLieAlgebraSO3(R);
-	trans = tr;
+	float shtot = 0.5f;
+	if (theta > 0.00001f)
+		shtot = sin(theta*0.5f) / theta;
 
-	float norm = length(rot);
-	float3x3 skewSymmetricW = VectorToSkewSymmetricMatrix(rot);
-	float3x3 V; V.setIdentity();
-	if (norm > FLT_EPSILON)	{
-		V += skewSymmetricW * ((1.0f - cos(norm)) / (norm * norm));
-		V += skewSymmetricW * skewSymmetricW * ((norm - sin(norm)) / (norm * norm * norm));
-		trans = V.getInverse() * tr;
-	}
+	// now do the rotation
+	float3 rot_half = rot;
+	rot_half *= -0.5f;
+	const float3x3 halfrotator = exp_rotation(rot_half);
+
+	trans = halfrotator * t;
+
+	if (theta > 0.001f)
+		trans -= rot * (dot(t, rot) * (1 - 2 * shtot) / dot(rot, rot));
+	else					 
+		trans -= rot * (dot(t, rot) / 24);
+	trans *= 1.0f / (2 * shtot);
 }
 
 __inline__ __device__ void poseToMatrix(const float3& rot, const float3& trans, float4x4& matrix, bool debug)
@@ -81,25 +168,49 @@ __inline__ __device__ void poseToMatrix(const float3& rot, const float3& trans, 
 	//!!!debugging
 	matrix.setIdentity();
 
-	float norm = length(rot);
-	float3 resTrans;
-	float3x3 resRot = LieAlgebraToLieGroupSO3(rot);
-	if (norm == 0.0f) {
-		resTrans = make_float3(0.0f);
+	float3 translation;
+	float3x3 rotation;
+
+	const float theta_sq = dot(rot, rot);
+	const float theta = std::sqrt(theta_sq);
+	float A, B;
+
+	float3 cr = cross(rot, trans);
+
+	if (theta_sq < 1e-8)
+	{
+		A = 1.0f - ONE_SIXTH * theta_sq;
+		B = 0.5f;
+		translation = trans + 0.5f * cr;
 	}
-	else {
-		float3x3 skewSymmetricW = VectorToSkewSymmetricMatrix(rot);
-		float3x3 V; V.setIdentity();
-		V += skewSymmetricW * ((1.0f - cos(norm)) / (norm * norm));
-		V += skewSymmetricW * skewSymmetricW * ((norm - sin(norm)) / (norm * norm * norm));
-		resTrans = V * trans;
+	else
+	{
+		float C;
+		if (theta_sq < 1e-6) {
+			C = ONE_SIXTH*(1.0f - ONE_TWENTIETH * theta_sq);
+			A = 1.0f - theta_sq * C;
+			B = 0.5f - 0.25f * ONE_SIXTH * theta_sq;
+		}
+		else {
+			const float inv_theta = 1.0f / theta;
+			A = sin(theta) * inv_theta;
+			B = (1 - cos(theta)) * (inv_theta * inv_theta);
+			C = (1 - A) * (inv_theta * inv_theta);
+		}
+
+		float3 w_cross = cross(rot, cr);
+		translation = trans + B * cr + C * w_cross;
 	}
+
+	// 3x3 rotation part:
+	rodrigues_so3_exp(rot, A, B, rotation);
+
 	//set rotation
-	matrix.setFloat3x3(resRot);
+	matrix.setFloat3x3(rotation);
 	//set translation
-	matrix.m14 = resTrans.x;
-	matrix.m24 = resTrans.y;
-	matrix.m34 = resTrans.z;
+	matrix.m14 = translation.x;
+	matrix.m24 = translation.y;
+	matrix.m34 = translation.z;
 }
 
 __inline__ __device__ float4x4 poseToMatrix(const float3& rot, const float3& trans)
